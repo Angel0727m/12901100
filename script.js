4945367f9548cfeee33660d3d8e7a7ed8d74202f
Angel0727m <angelrodriguez2012m@gmail.com>
2026-08-29 13:21:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 function mostrarTema(tema) {
 
+    // Ocultamos el inicio
     document.getElementById("inicio").style.display = "none";
 
+
+    // Ocultamos todas las páginas
     document.querySelectorAll(".pagina").forEach(function(pagina) {
+
         pagina.style.display = "none";
+
     });
 
+
+    // Mostramos solamente el tema elegido
     document.getElementById(tema).style.display = "block";
 }
 
 
+
 function volverInicio() {
 
+    // Ocultamos todas las páginas
     document.querySelectorAll(".pagina").forEach(function(pagina) {
+
         pagina.style.display = "none";
+
     });
 
+
+    // Mostramos nuevamente el menú
     document.getElementById("inicio").style.display = "block";
 }
