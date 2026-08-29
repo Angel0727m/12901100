dea540310c75fe10fe33ae938c2a12408221b46e
Angel0727m <angelrodriguez2012m@gmail.com>
2026-08-29 13:37:08 -0500

    Refactor mostrarTema and volverInicio functions

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 function mostrarTema(tema) {
 
+    // Ocultar el inicio
     document.getElementById("inicio").style.display = "none";
 
+    // Ocultar todas las páginas
     document.querySelectorAll(".pagina").forEach(function(pagina) {
         pagina.style.display = "none";
     });
 
+    // Mostrar el tema seleccionado
     document.getElementById(tema).style.display = "block";
 }
 
 
 function volverInicio() {
 
+    // Ocultar todas las páginas
     document.querySelectorAll(".pagina").forEach(function(pagina) {
         pagina.style.display = "none";
     });
 
+    // Mostrar el inicio
     document.getElementById("inicio").style.display = "block";
 }
